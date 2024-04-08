cc63a4ab172ce0c5fc3369057fa20cbfa9c4568e
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-08 13:25:18 -0400

    Feat: Apply JavaScript to buttons and test
    Applied query selectors and element listners
    to all buttons and test them.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,108 @@
+const btnZero = document.querySelector("#zero");
 const btnOne = document.querySelector("#one");
+const btnTwo = document.querySelector("#two");
+const btnThree = document.querySelector("#three");
+const btnFour = document.querySelector("#four");
+const btnFive = document.querySelector("#five");
+const btnSix = document.querySelector("#six");
+const btnSeven = document.querySelector("#seven");
+const btnEight = document.querySelector("#eight");
+const btnNine = document.querySelector("#nine");
+const btnPeriod = document.querySelector("#period");
+
+const btnClear = document.querySelector("#clear");
+const btnPlusMinus = document.querySelector("#plus-minus");
+const btnPercentage = document.querySelector("#percentage");
+const btnDivision = document.querySelector("#division");
+const btnMultiplication = document.querySelector("#multiplication");
+const btnSubtracation = document.querySelector("#subtraction");
+const btnAddition = document.querySelector("#addition");
+const btnEqual = document.querySelector("#equal");
+
+
+btnZero.addEventListener("click", function() {
+    console.log("0");
+});
 
 btnOne.addEventListener("click", function() {
     console.log("1");
 });
+
+btnTwo.addEventListener("click", function() {
+    console.log("2");
+});
+
+btnThree.addEventListener("click", function() {
+    console.log("3");
+});
+
+btnFour.addEventListener("click", function() {
+    console.log("4");
+});
+
+btnFive.addEventListener("click", function() {
+    console.log("5");
+});
+
+btnSix.addEventListener("click", function() {
+    console.log("6");
+});
+
+btnSeven.addEventListener("click", function() {
+    console.log("7");
+});
+
+btnEight.addEventListener("click", function() {
+    console.log("8");
+});
+
+btnNine.addEventListener("click", function() {
+    console.log("9");
+});
+
+btnPeriod.addEventListener("click", function() {
+    console.log(".");
+});
+
+btnClear.addEventListener("click", function() {
+    console.log("Clear");
+});
+
+btnPlusMinus.addEventListener("click", function() {
+    console.log("Plus Minus");
+});
+
+btnPercentage.addEventListener("click", function() {
+    console.log("%");
+});
+
+btnDivision.addEventListener("click", function() {
+    console.log("/");
+});
+
+btnMultiplication.addEventListener("click", function() {
+    console.log("*");
+});
+
+btnSubtracation.addEventListener("click", function() {
+    console.log("-");
+});
+
+btnAddition.addEventListener("click", function() {
+    console.log("+");
+});
+
+btnEqual.addEventListener("click", function() {
+    console.log("=");
+});
+
+
+
+
+
+
+
+
+
+
+
